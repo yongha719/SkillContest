1834dfffc79a200095b7596193121d86a5623fb7
Yongha <loha31707@gmail.com>
2023-03-20 16:03:36 +0900

    [용하] 뿡뿡

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -29,6 +28,7 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal"), 0) * Time.deltaTime * speed);
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -5, 5), -4);
     }
 
     private void Reset()
